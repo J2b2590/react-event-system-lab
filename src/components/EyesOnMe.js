84afda9ad8c387515d85cae5f0164815700cb7b0
J2b2590 <J2b2590@gmail.com>
2020-11-18 07:16:45 -0600

    finished lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,28 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class eyesOnMe extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    focusHandler = (e) => {
+        console.log("Good!")
+    }
+
+    blurHandler = () => {
+        console.log("Hey! Eyes on me!")
+    }
+
+
+    render() { 
+        return ( 
+            <div>
+                <button onFocus={this.focusHandler} onBlur={this.blurHandler}></button>
+            </div>
+         );
+    }
+}
+ 
+export default eyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,24 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+class keypad extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+
+    handleKeyup = (e) =>{
+        console.log("Entering password...")
+    }
+
+    render() { 
+        return ( 
+            <div>
+                <input type="password" onKeyUp={this.handleKeyup}></input>
+            </div>
+         );
+    }
+}
+ 
+export default keypad;
